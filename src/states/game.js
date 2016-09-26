5b8d547ac6c0c87f021b55905be72412cb48fd05
Jesper Lehtonen <jesle149@stundet.liu.se>
2016-09-26 18:24:52 +0200

    Shoot pencil with spacekey

--- src/states/game.js
+++ src/states/game.js
@@ -1,5 +1,85 @@
 //Documentation for Phaser's (2.5.0) states:: phaser.io/docs/2.5.0/Phaser.State.html
+
+var Pencil = function (game, key){
+            Phaser.Sprite.call(this, game, 0, 0, key);
+            this.texture.baseTexture.scaleMode = PIXI.scaleModes.NEAREST;
+            this.anchor.set(0.5);
+            this.checkWorldBounds = true;
+            this.outOfBoundsKill = true;
+            this.exists = false;
+
+            this.tracking = false;
+            this.scaleSpeed = 0;
+        };
+
+Pencil.prototype = Object.create(Phaser.Sprite.prototype);
+Pencil.prototype.constructor = Pencil;
+
+Pencil.prototype.fire = function (x, y, angle, speed, gx, gy) {
+    gx = gx || 0;
+    gy = gy || 0;
+    this.reset(x, y);
+    this.scale.set(1);
+
+    this.game.physics.arcade.velocityFromAngle(angle, speed, this.body.velocity);
+
+    this.angle = angle;
+
+    this.body.gravity.set(gx, gy);
+};
+
+Pencil.prototype.update = function () {
+
+if (this.tracking)
+{
+    this.rotation = Math.atan2(this.body.velocity.y, this.body.velocity.x);
+}
+if (this.scaleSpeed > 0)
+{
+    this.scale.x += this.scaleSpeed;
+    this.scale.y += this.scaleSpeed;
+}
+};
+
+var Weapon = {};
+
+
+Weapon.SingleBullet = function (game) {
+
+    Phaser.Group.call(this, game, game.world, 'Single Bullet', false, true, Phaser.Physics.ARCADE);
+
+    this.nextFire = 0;
+    this.bulletSpeed = 500;
+    this.fireRate = 300;
+    for (var i = 0; i < 64; i++)
+    {
+        this.add(new Pencil(game, 'pencil'), true);
+    }
+    return this;
+
+};
+
+Weapon.SingleBullet.prototype = Object.create(Phaser.Group.prototype);
+Weapon.SingleBullet.prototype.constructor = Weapon.SingleBullet;
+Weapon.SingleBullet.prototype.fire = function (source) {
+
+    if (this.game.time.time < this.nextFire) { return; }
+
+    var x = source.x + 10;
+    var y = source.y + 10;
+    this.getFirstExists(false).fire(x, y, 0, this.bulletSpeed, 0, 0);
+
+    this.nextFire = this.game.time.time + this.fireRate;
+
+};
+
+
+
+
+
 class Game extends Phaser.State {
+
+
 
     //initialization code in the constructor
     constructor(game, parent) {
@@ -22,6 +102,10 @@ class Game extends Phaser.State {
             "w": 6,
         };
         this.update_counter = 0;
+
+        this.weapons = [];
+        this.currentWeapon = 0;
+        this.weaponName = null;
     }
 
     //Load operations (uses Loader), method called first
@@ -29,6 +113,7 @@ class Game extends Phaser.State {
         this.game.load.image('sky', 'assets/sky.png');
         this.game.load.image('ground', 'assets/platform.png');
         this.game.load.image('star', 'assets/star.png');
+        this.game.load.image('pencil', 'assets/pencil.png');
         this.game.load.spritesheet('dude', 'assets/student.png', 40, 40);
     }
 
@@ -85,6 +170,13 @@ class Game extends Phaser.State {
         this.player.animations.add('leftdown', [14, 15, 16], 10, true);
         this.player.animations.add('rightdown', [19, 20, 23], 10, true);
         this.cursors = this.game.input.keyboard.createCursorKeys();
+        this.input.keyboard.addKeyCapture([ Phaser.Keyboard.SPACEBAR ]);
+
+
+        this.weapons.push(new Weapon.SingleBullet(this.game));
+        this.currentWeapon = 0;
+
+
     }
 
     //Code ran on each frame of game
@@ -92,6 +184,11 @@ class Game extends Phaser.State {
         this.game.physics.arcade.collide(this.player, this.platforms);
         //  Reset the players velocity (movement)
         this.move();
+
+        if (this.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR))
+        {
+            this.weapons[this.currentWeapon].fire(this.player);
+        }
 
     }
 
@@ -231,6 +328,8 @@ class Game extends Phaser.State {
         }
         console.log(this.update_counter);
     }
+
+
 
     //Called when game is paused
     paused() {
