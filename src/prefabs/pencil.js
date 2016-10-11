571182877332c1a2b5475c7b2dda0a7ca3cf80fc
jonathan <jonathan@jonathananderson.se>
2016-10-11 20:22:46 +0200

    Moved pencil to a prefab

---
+++ src/prefabs/pencil.js
@@ -0,0 +1,71 @@
+
+//Documentation for Phaser's (2.5.0) states:: phaser.io/docs/2.5.0/Phaser.State.html
+class Pencil extends Phaser.Sprite {
+
+  //initialization code in the constructor
+  constructor(game, x, y, frame) {
+    super(game, x, y, 'pencil', frame);
+    this.texture.baseTexture.scaleMode = PIXI.scaleModes.NEAREST;
+    this.anchor.set(0.5);
+    this.checkWorldBounds = true;
+    this.outOfBoundsKill = true;
+    this.exists = false;
+
+    this.tracking = false;
+    this.scaleSpeed = 0;
+  }
+
+  fire(x, y, angle, speed, gx, gy) {
+    gx = gx || 0;
+    gy = gy || 0;
+    this.reset(x, y);
+    this.scale.set(1);
+
+    this.game.physics.arcade.velocityFromAngle(angle, speed, this.body.velocity);
+
+    this.angle = angle;
+
+    this.body.gravity.set(gx, gy);
+  };
+  //Load operations (uses Loader), method called first
+  preload() {
+
+  }
+
+  //Setup code, method called after preload
+  create() {
+
+  }
+
+  //Code ran on each frame of game
+  update() {
+
+    if (this.tracking)
+    {
+      this.rotation = Math.atan2(this.body.velocity.y, this.body.velocity.x);
+    }
+    if (this.scaleSpeed > 0)
+    {
+      this.scale.x += this.scaleSpeed;
+      this.scale.y += this.scaleSpeed;
+    }
+  }
+
+  //Called when game is paused
+  paused() {
+
+  }
+
+  //You're able to do any final post-processing style effects here.
+  render() {
+
+  }
+
+  //Called when switching to a new state
+  shutdown() {
+
+  }
+
+}
+
+export default Pencil;
--- src/states/game.js
+++ src/states/game.js
@@ -1,45 +1,5 @@
 //Documentation for Phaser's (2.5.0) states:: phaser.io/docs/2.5.0/Phaser.State.html
-
-var Pencil = function (game, key){
-            Phaser.Sprite.call(this, game, 0, 0, key);
-            this.texture.baseTexture.scaleMode = PIXI.scaleModes.NEAREST;
-            this.anchor.set(0.5);
-            this.checkWorldBounds = true;
-            this.outOfBoundsKill = true;
-            this.exists = false;
-
-            this.tracking = false;
-            this.scaleSpeed = 0;
-        };
-
-Pencil.prototype = Object.create(Phaser.Sprite.prototype);
-Pencil.prototype.constructor = Pencil;
-
-Pencil.prototype.fire = function (x, y, angle, speed, gx, gy) {
-    gx = gx || 0;
-    gy = gy || 0;
-    this.reset(x, y);
-    this.scale.set(1);
-
-    this.game.physics.arcade.velocityFromAngle(angle, speed, this.body.velocity);
-
-    this.angle = angle;
-
-    this.body.gravity.set(gx, gy);
-};
-
-Pencil.prototype.update = function () {
-
-if (this.tracking)
-{
-    this.rotation = Math.atan2(this.body.velocity.y, this.body.velocity.x);
-}
-if (this.scaleSpeed > 0)
-{
-    this.scale.x += this.scaleSpeed;
-    this.scale.y += this.scaleSpeed;
-}
-};
+import Pencil from '../prefabs/pencil';
 
 var Weapon = {};
 
